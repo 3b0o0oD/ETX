bcaa5f56b9f59c5af52873ca64c189ba2823c0e2
3b0o0oD <white.angel.3j@gmail.com>
2025-02-25 00:55:38 +0400

    modified:   index.html 	renamed:    src/assets/CCTV.jpg -> public/CCTV.jpg 	renamed:    src/assets/Smart-Keypad.jpg -> public/Smart-Keypad.jpg 	renamed:    src/assets/etx-logo-grey.svg -> public/etx-logo-grey.svg 	renamed:    src/assets/etx-logo.svg -> public/etx-logo.svg 	renamed:    src/assets/video-bg-reverse.mp4 -> public/video-bg-reverse.mp4 	renamed:    src/assets/video-bg.mp4 -> public/video-bg.mp4 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
-  base: '/ETX/',
-})
+  base: "https://3b0o0od.github.io/ETX/",
+  plugins: [tailwindcss(), react()],
+});
