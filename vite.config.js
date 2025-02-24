63a26f8ea8e4ec0b3962875c20ae88aa94d3d516
3b0o0oD <white.angel.3j@gmail.com>
2025-02-25 01:34:48 +0400

    modified:   index.html 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://3b0o0od.github.io/ETX/",
+  base: "/ETX/",
   plugins: [tailwindcss(), react()],
 });
