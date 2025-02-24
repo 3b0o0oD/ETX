5e56fc0b584649f2f800b6076928cb51e164527e
3b0o0oD <white.angel.3j@gmail.com>
2025-02-25 02:52:51 +0400

    modified:   index.html 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ETX/", // 👈 Fix the base path
   plugins: [tailwindcss(), react()],
   build: {
     outDir: "dist",
