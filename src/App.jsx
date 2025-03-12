d08e12f95240519c018958a06925519ae196e2d9
3b0o0oD <white.angel.3j@gmail.com>
2025-03-12 21:46:32 +0400

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { I18nextProvider } from "react-i18next";
-import i18n from "@/i18n";
+import i18n from "./i18n";
 import { motion, useScroll, useTransform } from "framer-motion";
 import Footer from "@/Footer";
 import Navbar from "@/Navbar";
@@ -11,7 +11,6 @@ import Contact from "@/Contact.jsx";
 import { useTranslation } from "react-i18next";
 import Lenis from "@studio-freight/lenis";
 import { ArrowDown } from "lucide-react";
-import i18n from "i18next";
 
 export default function HomePage() {
   const { t } = useTranslation();
