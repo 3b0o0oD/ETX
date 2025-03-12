df94ab86e07a2f96c5f3fc2b7c566bd2594591d1
3b0o0oD <white.angel.3j@gmail.com>
2025-03-12 21:47:57 +0400

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n";
+import i18n from "@/i18n";
 import { motion, useScroll, useTransform } from "framer-motion";
 import Footer from "@/Footer";
 import Navbar from "@/Navbar";
