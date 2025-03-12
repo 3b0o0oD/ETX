9d316c2a47cd0a27f82994b6dac7a3be07f4f2cf
3b0o0oD <white.angel.3j@gmail.com>
2025-03-12 21:26:47 +0400

    modified:   src/Featurecard.jsx

--- src/Featurecard.jsx
+++ src/Featurecard.jsx
@@ -4,7 +4,11 @@ import React from "react";
 const FeatureCard = ({ title, img, description, moreContent }) => {
   return (
     <div className="p-6 rounded-lg shadow-md">
-      <img src={img} alt={title} className="w-full h-48 object-cover rounded-md mb-4" />
+      <img
+        src={img}
+        alt={title}
+        className="w-full h-48 object-cover rounded-md mb-4"
+      />
       <h3 className="text-xl font-semibold mb-2">{title}</h3>
       <p className="text-gray-400 mb-4">{description}</p>
       {/* {moreContent && (
