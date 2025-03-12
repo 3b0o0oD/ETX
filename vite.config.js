cc6daabbc2b9eb7b9e10f894f52ba1872be481d4
3b0o0oD <white.angel.3j@gmail.com>
2025-03-12 21:34:50 +0400

    modified:   src/App.jsx 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "", // 👈 Fix the base path
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  base: "",
   plugins: [tailwindcss(), react()],
-
 });
-
